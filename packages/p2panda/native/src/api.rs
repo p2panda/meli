1c217e152febf418529b95ff32c3715e4f4b2c5f
adz <x12@adz.garden>
2023-07-10 14:34:20 +0200

    Remove build script and use command line tool instead

--- packages/p2panda/native/src/api.rs
+++ packages/p2panda/native/src/api.rs
@@ -1,5 +1,16 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+use anyhow::Result;
+use p2panda_rs::entry::encode::sign_and_encode_entry;
+use p2panda_rs::entry::EntryBuilder;
+use p2panda_rs::identity::KeyPair;
+use p2panda_rs::operation::encode::encode_operation;
+use p2panda_rs::operation::OperationBuilder;
+use flutter_rust_bridge::frb;
+
+#[frb(mirror(KeyPair))]
+pub struct _KeyPair(KeyPair);
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,29 +1,40 @@
 #!/usr/bin/env bash
 
-# Build libraries for Android from our Rust codebase, with support for
-# generating the correct `jniLibs` directory structure.
+# Generate FFI bindings from Rust and build native libraries for Android.
 
+echo "◆ Install Rust toolchain dependencies"
+echo
+
+# Set up required Cargo toolbelt applications and Android compilation targets
+cargo install cargo-ndk
+cargo install flutter_rust_bridge_codegen
+rustup target add \
+        aarch64-linux-android \
+        armv7-linux-androideabi \
+        x86_64-linux-android \
+        i686-linux-android
+
+echo "◆ Generate FFI bindings"
+echo
+
+# See: https://cjycode.com/flutter_rust_bridge/integrate/deps.html#system-dependencies
+CPATH="$(clang -v 2>&1 | grep "Selected GCC installation" | rev | cut -d' ' -f1 | rev)/include" \
+        flutter_rust_bridge_codegen \
+                            --inline-rust \
+                            --skip-add-mod-to-lib \
+                            --rust-input packages/p2panda/native/src/api.rs \
+                            --dart-output packages/p2panda/lib/src/bridge_generated.dart
+
+echo
 echo "◆ Create folders"
 echo
 
 BUILD_DIR=platform-build
 JNI_DIR=jniLibs
 
-# Make sure that target folders exist
 mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 mkdir -p $JNI_DIR
-
-echo "◆ Install Rust toolchain dependencies"
-echo
-
-# Set up cargo-ndk and Android compilation targets
-cargo install cargo-ndk
-rustup target add \
-        aarch64-linux-android \
-        armv7-linux-androideabi \
-        x86_64-linux-android \
-        i686-linux-android
 
 echo "◆ Build project"
 echo
@@ -40,7 +51,7 @@ cargo ndk -o $JNI_DIR \
 echo "◆ Archive binaries"
 echo
 
-# Archive the dynamic libs
+# Archive the dynamic libraries
 cd $JNI_DIR
 tar -czvf ../android.tar.gz *
 cd -
