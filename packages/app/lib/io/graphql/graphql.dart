a71fc72b10d6d1df19e45c0f1838c419d69b275f
adz <x12@adz.garden>
2023-07-18 11:32:32 +0200

    Disable GraphQL client caching

--- packages/app/lib/io/graphql/graphql.dart
+++ packages/app/lib/io/graphql/graphql.dart
@@ -5,6 +5,19 @@ import 'package:graphql/client.dart';
 /// GraphQL endpoint URL.
 const String HTTP_ENDPOINT = 'http://localhost:2020/graphql';
 
+final policies = Policies(
+  fetch: FetchPolicy.networkOnly,
+);
+
 /// GraphQL client making requests against our locally hosted node API.
-final GraphQLClient client =
-    GraphQLClient(link: HttpLink(HTTP_ENDPOINT), cache: GraphQLCache());
+final GraphQLClient client = GraphQLClient(
+  link: HttpLink(HTTP_ENDPOINT),
+  cache: GraphQLCache(),
+  // Disable caching by overriding default policies. See:
+  // https://github.com/zino-hofmann/graphql-flutter/issues/692
+  defaultPolicies: DefaultPolicies(
+    watchQuery: policies,
+    query: policies,
+    mutate: policies,
+  ),
+);
