13619871844b7cfca1b813deb7eeb88660de5145
Sam Andreae <contact@samandreae.com>
2023-09-15 14:04:02 +0100

    Add optional title to MeliCard

--- packages/app/lib/ui/widgets/card.dart
+++ packages/app/lib/ui/widgets/card.dart
@@ -5,8 +5,32 @@ import 'package:app/ui/colors.dart';
 
 class MeliCard extends StatelessWidget {
   final Widget child;
+  final String? title;
 
-  MeliCard({super.key, required this.child});
+  MeliCard({super.key, required this.child, this.title});
+
+  Widget _title() {
+    if (this.title != null) {
+      return Card(
+        child: Container(
+          alignment: AlignmentDirectional.centerStart,
+          padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+          width: double.infinity,
+          height: 60,
+          child: Text(
+            this.title.toString(),
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      );
+    }
+
+    return SizedBox(height: 0.0);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +47,7 @@ class MeliCard extends StatelessWidget {
       ),
       child: Container(
         padding: const EdgeInsets.all(1),
-        child: this.child,
+        child: Column(children: [this._title(), this.child]),
       ),
     );
   }
