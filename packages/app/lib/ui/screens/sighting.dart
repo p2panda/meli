9647177fce649a6740605921cdfc690c6414b587
Sam Andreae <contact@samandreae.com>
2023-09-15 18:03:58 +0100

    WIP: Sighting screen with editable cards

--- packages/app/lib/ui/screens/sighting.dart
+++ packages/app/lib/ui/screens/sighting.dart
@@ -2,19 +2,52 @@
 
 import 'package:flutter/material.dart';
 import 'package:app/ui/widgets/scaffold.dart';
+import 'package:app/data.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+
+import '../widgets/editable_card.dart';
 
 class SightingScreen extends StatefulWidget {
-  SightingScreen({super.key});
+  final String id;
+
+  SightingScreen({super.key, required this.id});
 
   @override
   State<SightingScreen> createState() => _SightingScreenState();
 }
 
 class _SightingScreenState extends State<SightingScreen> {
+  final _formKey = GlobalKey<FormBuilderState>();
+
+  Map<String, String> _sighting() {
+    return sightings.where((element) => element['id'] == this.widget.id).first;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MeliScaffold(
         title: 'New Sighting',
-        body: Container(padding: EdgeInsets.all(10.0), child: Text('Hello')));
+        body: SingleChildScrollView(
+            child: Column(children: <Widget>[
+          FormBuilder(
+            key: _formKey,
+            // enabled: false,
+            onChanged: () {
+              _formKey.currentState!.save();
+              debugPrint(_formKey.currentState!.value.toString());
+            },
+            autovalidateMode: AutovalidateMode.disabled,
+            skipDisabled: true,
+            child: Column(
+              children: <Widget>[
+                EditableCard(
+                    title: 'Name', fields: {'Name': this._sighting()['name']!}),
+                EditableCard(
+                    title: 'Species',
+                    fields: {'Species': this._sighting()['species']!})
+              ],
+            ),
+          )
+        ])));
   }
 }
---
+++ packages/app/lib/ui/widgets/editable_card.dart
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import 'package:app/ui/widgets/card.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+
+class EditableCard extends StatefulWidget {
+  final Map<String, String> fields;
+  final String? title;
+
+  EditableCard({super.key, this.title, required this.fields});
+
+  @override
+  State<EditableCard> createState() => _EditableCardState();
+}
+
+class _EditableCardState extends State<EditableCard> {
+  bool editMode = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MeliCard(
+      title: this.widget.title,
+      child: Column(
+        children: <Widget>[
+          ...this.widget.fields.entries.map((entry) => FormBuilderTextField(
+                name: entry.key,
+                initialValue: entry.value,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12)),
+                ),
+              ))
+        ],
+      ),
+    );
+  }
+}
