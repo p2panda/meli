a47070b9b80d94e927ecdf2632209d67abe0ba33
Sam Andreae <contact@samandreae.com>
2023-09-15 14:04:13 +0100

    New color: white

--- packages/app/lib/ui/colors.dart
+++ packages/app/lib/ui/colors.dart
@@ -7,4 +7,5 @@ abstract final class MeliColors {
   static const Color electric = Color(0xff95fff2);
   static const Color grass = Color(0x0095ffd9);
   static const Color magnolia = Color(0xffeaddff);
+  static const Color white = Color(0xffffffff);
 }
