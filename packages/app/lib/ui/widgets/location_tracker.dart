e764211270537f8515eab1b3c8b8b98f124f5549
adz <x12@adz.garden>
2023-09-27 12:53:08 +0200

    Introduce LocationTracker widget

---
+++ packages/app/lib/ui/widgets/location_tracker.dart
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import 'package:flutter/material.dart';
+
+class LocationTracker extends StatelessWidget {
+  LocationTracker({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Location Tracker');
+  }
+}
