6f3216ac8bfe79b51448940da7b6ac5a6ea71ac8
adz <x12@adz.garden>
2023-09-27 12:50:35 +0200

    Remove unused import

--- packages/app/lib/ui/screens/settings.dart
+++ packages/app/lib/ui/screens/settings.dart
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-import 'package:app/ui/widgets/simple_card.dart';
 import 'package:flutter/material.dart';
 
 import 'package:app/ui/widgets/expandable_card.dart';
