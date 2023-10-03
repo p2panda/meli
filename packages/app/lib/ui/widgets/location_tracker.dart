9526ab1411a37b8e9bf617fdca632411680d453e
adz <x12@adz.garden>
2023-10-03 09:54:21 +0200

    Separate logic and ui within same file

--- packages/app/lib/ui/widgets/location_tracker.dart
+++ packages/app/lib/ui/widgets/location_tracker.dart
@@ -8,17 +8,30 @@ import 'package:geolocator/geolocator.dart';
 
 import 'package:app/io/geolocation.dart';
 
-enum TrackerStatus {
+enum LocationTrackerStatus {
   Standby,
   Waiting,
   Active,
   Failure,
 }
 
+typedef AddLocation = void Function();
+
+typedef RemoveLocation = void Function();
+
+typedef LocationTrackerBuilder = Widget Function(
+  Position? position,
+  LocationTrackerStatus status, {
+  String? errorMessage,
+  AddLocation? addLocation,
+  RemoveLocation? removeLocation,
+});
+
 class LocationTracker extends StatefulWidget {
   final ValueChanged<Position?>? onPositionChanged;
+  final LocationTrackerBuilder builder;
 
-  LocationTracker({super.key, this.onPositionChanged});
+  LocationTracker({super.key, required this.builder, this.onPositionChanged});
 
   @override
   State<LocationTracker> createState() => _LocationTrackerState();
@@ -32,10 +45,87 @@ class _LocationTrackerState extends State<LocationTracker> {
   Position? _position;
 
   /// Current status of the position tracker.
-  TrackerStatus _status = TrackerStatus.Standby;
+  LocationTrackerStatus _status = LocationTrackerStatus.Standby;
 
   /// Keep track of the last known error.
   String? _errorMessage;
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      this._locale = AppLocalizations.of(context);
+    });
+  }
+
+  Position? getPosition() {
+    return this._position;
+  }
+
+  void _updatePosition(Position? value) {
+    this._position = value;
+
+    if (widget.onPositionChanged != null) {
+      widget.onPositionChanged!.call(value);
+    }
+  }
+
+  void _addLocation() async {
+    setState(() {
+      this._status = LocationTrackerStatus.Waiting;
+      this._errorMessage = null;
+    });
+
+    try {
+      final position = await determinePosition();
+      this._updatePosition(position);
+      this._status = LocationTrackerStatus.Active;
+    } on PermissionDeniedException {
+      this._errorMessage = _locale!.locationErrorPermissionDenied;
+    } on LocationServiceDisabledException {
+      this._errorMessage = _locale!.locationErrorServiceDisabled;
+    } on TimeoutException {
+      this._errorMessage = _locale!.locationErrorTimeout;
+    } catch (error) {
+      this._errorMessage = _locale!.locationErrorUnknown;
+    } finally {
+      this._status = this._errorMessage != null
+          ? LocationTrackerStatus.Failure
+          : this._status;
+      setState(() {});
+    }
+  }
+
+  void _removeLocation() {
+    setState(() {
+      this._status = LocationTrackerStatus.Standby;
+    });
+
+    this._updatePosition(null);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.builder(this._position, this._status,
+        errorMessage: this._errorMessage,
+        addLocation: this._addLocation,
+        removeLocation: this._removeLocation);
+  }
+}
+
+class LocationTrackerInput extends StatefulWidget {
+  final ValueChanged<Position?>? onPositionChanged;
+
+  LocationTrackerInput({super.key, this.onPositionChanged});
+
+  @override
+  State<LocationTrackerInput> createState() => _LocationTrackerInputState();
+}
+
+class _LocationTrackerInputState extends State<LocationTrackerInput> {
+  final GlobalKey<_LocationTrackerState> trackerKey = GlobalKey();
+  AppLocalizations? _locale;
 
   /// User confirmed that they want to track their location. This does not have
   /// anything to do with the device's permission settings but merely with the
@@ -51,52 +141,11 @@ class _LocationTrackerState extends State<LocationTracker> {
     });
   }
 
-  Future<Position?> getPosition() async {
-    return this._position;
+  Position? getPosition() {
+    return this.trackerKey.currentState!.getPosition();
   }
 
-  void _updatePosition(Position? value) {
-    this._position = value;
-
-    if (widget.onPositionChanged != null) {
-      widget.onPositionChanged!.call(value);
-    }
-  }
-
-  void _addLocation() async {
-    setState(() {
-      this._status = TrackerStatus.Waiting;
-      this._errorMessage = null;
-    });
-
-    try {
-      final position = await determinePosition();
-      this._updatePosition(position);
-      this._status = TrackerStatus.Active;
-    } on PermissionDeniedException {
-      this._errorMessage = _locale!.locationErrorPermissionDenied;
-    } on LocationServiceDisabledException {
-      this._errorMessage = _locale!.locationErrorServiceDisabled;
-    } on TimeoutException {
-      this._errorMessage = _locale!.locationErrorTimeout;
-    } catch (error) {
-      this._errorMessage = _locale!.locationErrorUnknown;
-    } finally {
-      this._status =
-          this._errorMessage != null ? TrackerStatus.Failure : this._status;
-      setState(() {});
-    }
-  }
-
-  void _removeLocation() {
-    setState(() {
-      this._status = TrackerStatus.Standby;
-    });
-
-    this._updatePosition(null);
-  }
-
-  Widget _standby() {
+  Widget _standby(AddLocation? addLocation) {
     return Column(children: [
       Text('Do you want to add a GPS location?'),
       TextButton(
@@ -106,7 +155,7 @@ class _LocationTrackerState extends State<LocationTracker> {
               _hasUserGivenConsent = true;
             });
 
-            _addLocation();
+            addLocation!();
           }),
     ]);
   }
@@ -115,13 +164,9 @@ class _LocationTrackerState extends State<LocationTracker> {
     return Text('Retreiving location ...');
   }
 
-  Widget _success() {
-    if (this._position == null) {
-      return SizedBox.shrink();
-    }
-
-    final latitude = this._position!.latitude;
-    final longitude = this._position!.longitude;
+  Widget _success(Position position, RemoveLocation? removeLocation) {
+    final latitude = position.latitude;
+    final longitude = position.longitude;
 
     return Column(children: [
       Text('Location recorded: $latitude $longitude'),
@@ -131,36 +176,48 @@ class _LocationTrackerState extends State<LocationTracker> {
               _hasUserGivenConsent = false;
             });
 
-            _removeLocation();
+            removeLocation!();
           },
           child: Text('Remove location'))
     ]);
   }
 
-  Widget _failed() {
+  Widget _failed(String errorMessage, AddLocation? addLocation) {
     return Column(children: [
-      Text(this._errorMessage!),
+      Text(errorMessage),
       Text(this._locale!.locationTryAgain),
       TextButton(
-          onPressed: _addLocation,
+          onPressed: addLocation,
           child: Text(this._locale!.locationTryAgainAction))
     ]);
   }
 
   @override
   Widget build(BuildContext context) {
-    if (!this._hasUserGivenConsent) {
-      return this._standby();
-    }
+    return LocationTracker(
+        key: trackerKey,
+        builder: (Position? position, LocationTrackerStatus status,
+            {String? errorMessage,
+            AddLocation? addLocation,
+            RemoveLocation? removeLocation}) {
+          if (!this._hasUserGivenConsent) {
+            return this._standby(addLocation);
+          }
 
-    switch (this._status) {
-      case TrackerStatus.Standby:
-      case TrackerStatus.Waiting:
-        return this._waiting();
-      case TrackerStatus.Active:
-        return this._success();
-      case TrackerStatus.Failure:
-        return this._failed();
-    }
+          switch (status) {
+            case LocationTrackerStatus.Standby:
+            case LocationTrackerStatus.Waiting:
+              return this._waiting();
+            case LocationTrackerStatus.Active:
+              return this._success(position!, removeLocation);
+            case LocationTrackerStatus.Failure:
+              return this._failed(errorMessage!, addLocation);
+          }
+        },
+        onPositionChanged: (Position? value) {
+          if (widget.onPositionChanged != null) {
+            widget.onPositionChanged!.call(value);
+          }
+        });
   }
 }
