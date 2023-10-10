c0b90f827dad55bcdb61c3c60035bf1e443bed8d
adz <x12@adz.garden>
2023-10-10 11:33:58 +0200

    Make color of FABs configurable

--- packages/app/lib/ui/widgets/fab.dart
+++ packages/app/lib/ui/widgets/fab.dart
@@ -2,12 +2,18 @@
 
 import 'package:flutter/material.dart';
 
+import 'package:app/ui/colors.dart';
+
 class MeliFloatingActionButton extends StatefulWidget {
   final Icon icon;
   final VoidCallback onPressed;
+  final Color backgroundColor;
 
   MeliFloatingActionButton(
-      {super.key, required this.icon, required this.onPressed});
+      {super.key,
+      required this.icon,
+      required this.onPressed,
+      this.backgroundColor = MeliColors.magnolia});
 
   @override
   State<MeliFloatingActionButton> createState() =>
@@ -22,6 +28,7 @@ class _MeliFloatingActionButtonState extends State<MeliFloatingActionButton> {
       alignment: Alignment.bottomCenter,
       child: FloatingActionButton(
         foregroundColor: Colors.black,
+        backgroundColor: widget.backgroundColor,
         heroTag: null,
         child: widget.icon,
         shape: const CircleBorder(),
