ae8f517d311a0223b64950515b7925fc4b0af583
adz <x12@adz.garden>
2023-10-11 16:34:19 +0200

    Use material instead of cupertino

--- packages/app/lib/main.dart
+++ packages/app/lib/main.dart
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'package:app/app.dart';
