28dba1fa3df4d5001aa7a38347224fa4f670fc8e
adz <x12@adz.garden>
2023-10-16 21:25:52 +0200

    Separate into multiple widgets, use FutureBuilder

--- packages/app/lib/ui/screens/settings.dart
+++ packages/app/lib/ui/screens/settings.dart
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
-import 'package:app/app.dart';
-import 'package:app/ui/widgets/simple_card.dart';
 import 'package:flutter/material.dart';
 import 'package:device_info_plus/device_info_plus.dart';
+
+import 'package:app/app.dart';
+import 'package:app/ui/colors.dart';
+import 'package:app/ui/widgets/expandable_card.dart';
 import 'package:app/ui/widgets/scaffold.dart';
 
 class SettingsScreen extends StatefulWidget {
@@ -16,28 +17,8 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
-  static final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
-  AndroidDeviceInfo? _deviceData;
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  Future<void> initPlatformState() async {
-    final deviceData = await deviceInfoPlugin.androidInfo;
-
-    if (!mounted) return;
-
-    setState(() {
-      _deviceData = deviceData;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    final locale = Localizations.localeOf(context);
     final t = AppLocalizations.of(context)!;
 
     return MeliScaffold(
@@ -46,40 +27,80 @@ class _SettingsScreenState extends State<SettingsScreen> {
           padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
           child: SingleChildScrollView(
               padding: EdgeInsets.only(top: 30.0, bottom: 20.0),
-              child: Column(children: [
-                SimpleCard(
-                  title: t.settingsLanguages,
-                  child: DropdownMenu<Locale>(
-                    initialSelection: locale,
-                    onSelected: (Locale? value) {
-                      // This is called when the user selects an item.
-                      final app =
-                          context.findAncestorStateOfType<MeliAppState>()!;
-                      app.changeLocale(value!);
-                    },
-                    dropdownMenuEntries: [
-                      DropdownMenuEntry<Locale>(
-                          value: Locale('en'), label: t.settingsEnglish),
-                      DropdownMenuEntry<Locale>(
-                          value: Locale('pt'), label: t.settingsPortuguese)
-                    ],
-                  ),
-                ),
-                if (_deviceData != null)
-                  SimpleCard(
-                    title: t.settingsSystemInformation,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Text('Device: ${_deviceData!.device}'),
-                        Text(
-                            'Android SDK Version: ${_deviceData!.version.sdkInt}'),
-                        Text(
-                            'Android Build Version: ${_deviceData!.version.release}'),
-                      ],
-                    ),
-                  ),
+              child: Wrap(runSpacing: 20.0, children: [
+                LocaleSettings(),
+                SystemInfo(),
               ])),
         ));
+  }
+}
+
+class LocaleSettings extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final currentLocale = Localizations.localeOf(context);
+    final t = AppLocalizations.of(context)!;
+
+    return ExpandableCard(
+        title: t.settingsLanguages,
+        child: DropdownMenu<Locale>(
+            initialSelection: currentLocale,
+            onSelected: (Locale? value) {
+              final app = context.findAncestorStateOfType<MeliAppState>()!;
+              app.changeLocale(value!);
+            },
+            dropdownMenuEntries: [
+              DropdownMenuEntry<Locale>(
+                  value: Locale('en'), label: t.settingsEnglish),
+              DropdownMenuEntry<Locale>(
+                  value: Locale('pt'), label: t.settingsPortuguese)
+            ]));
+  }
+}
+
+class SystemInfo extends StatefulWidget {
+  @override
+  State<SystemInfo> createState() => _SystemInfoState();
+}
+
+class _SystemInfoState extends State<SystemInfo> {
+  static final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+
+  Future<AndroidDeviceInfo> get _getSystemInfo async {
+    return await deviceInfoPlugin.androidInfo;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final t = AppLocalizations.of(context)!;
+
+    return ExpandableCard(
+      title: t.settingsSystemInformation,
+      child: FutureBuilder<AndroidDeviceInfo>(
+          future: _getSystemInfo,
+          builder: (BuildContext context,
+              AsyncSnapshot<AndroidDeviceInfo> snapshot) {
+            if (snapshot.hasData) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Device: ${snapshot.data!.device}'),
+                  Text('Android SDK Version: ${snapshot.data!.version.sdkInt}'),
+                  Text(
+                      'Android Build Version: ${snapshot.data!.version.release}'),
+                ],
+              );
+            } else if (snapshot.hasError) {
+              return Text(
+                  'Something went wrong, could not retrieve system information');
+            } else {
+              return Container(
+                padding: const EdgeInsets.all(20.0),
+                child: const Center(
+                    child: CircularProgressIndicator(color: MeliColors.black)),
+              );
+            }
+          }),
+    );
   }
 }
