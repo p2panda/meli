f1a17bc083e8091457ef09a21fa489977474b69b
Sam Andreae <contact@samandreae.com>
2023-10-16 12:11:49 +0100

    Get and show all device data

--- packages/app/lib/ui/screens/settings.dart
+++ packages/app/lib/ui/screens/settings.dart
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 import 'package:flutter/material.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 
 import 'package:app/ui/widgets/expandable_card.dart';
 import 'package:app/ui/widgets/scaffold.dart';
@@ -13,6 +14,25 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  static final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+  AndroidDeviceInfo? _deviceData;
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  Future<void> initPlatformState() async {
+    final deviceData = await deviceInfoPlugin.androidInfo;
+
+    if (!mounted) return;
+
+    setState(() {
+      _deviceData = deviceData;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MeliScaffold(
@@ -21,34 +41,27 @@ class _SettingsScreenState extends State<SettingsScreen> {
           padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
           child: SingleChildScrollView(
               padding: EdgeInsets.only(top: 30.0, bottom: 20.0),
-              child: SettingsList()),
+              child: Wrap(spacing: 0.0, runSpacing: 20.0, children: [
+                ExpandableCard(
+                  title: 'Language',
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [Text('portuguese'), Text('english')]),
+                ),
+                ExpandableCard(
+                  title: 'Advanced Settings',
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Don\'t click here'),
+                        Text('Don\'t click here either!')
+                      ]),
+                ),
+                ExpandableCard(
+                  title: 'System Information',
+                  child: Text(_deviceData.toString()),
+                ),
+              ])),
         ));
-  }
-}
-
-class SettingsList extends StatelessWidget {
-  final List<Widget> menuListItems = [
-    ExpandableCard(
-      title: 'Language',
-      child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [Text('portuguese'), Text('english')]),
-    ),
-    ExpandableCard(
-      title: 'Advanced Settings',
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        Text('Don\'t click here'),
-        Text('Don\'t click here either!')
-      ]),
-    ),
-    ExpandableCard(
-      title: 'System Information',
-      child: Text('...'),
-    ),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Wrap(spacing: 0.0, runSpacing: 20.0, children: menuListItems);
   }
 }
