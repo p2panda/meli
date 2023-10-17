158ed227c794f2f0b18deead28a04e9f1f6b3770
adz <x12@adz.garden>
2023-10-17 12:22:46 +0200

    Prepare basic widget structure

--- packages/app/lib/ui/widgets/species_field.dart
+++ packages/app/lib/ui/widgets/species_field.dart
@@ -1,21 +1,58 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'package:app/models/species.dart';
 import 'package:app/ui/widgets/autocomplete.dart';
+import 'package:app/ui/widgets/editable_card.dart';
+import 'package:app/ui/widgets/read_only_value.dart';
 
 typedef OnUpdate = void Function(AutocompleteItem?);
 
-class SpeciesField extends StatelessWidget {
+class SpeciesField extends StatefulWidget {
   final Species? current;
   final OnUpdate onUpdate;
 
   SpeciesField(this.current, {super.key, required this.onUpdate});
 
   @override
+  State<SpeciesField> createState() => _SpeciesFieldState();
+}
+
+class _SpeciesFieldState extends State<SpeciesField> {
+  /// Flag indicating if we're currently editing the field or not.
+  bool isEditMode = false;
+
+  void _submit() async {
+    // @TODO
+  }
+
+  void _toggleEditMode() {
+    setState(() {
+      isEditMode = !isEditMode;
+
+      // If we flip from edit mode to read-only mode we interpret this as a
+      // "submit" action by the user
+      if (!isEditMode) {
+        _submit();
+      }
+    });
+  }
+
+  Widget _editableValue() {
+    return Text('@TODO');
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    String? displayValue =
+        widget.current == null ? null : widget.current!.species.name;
+
+    return EditableCard(
+        title: AppLocalizations.of(context)!.speciesCardTitle,
+        isEditMode: isEditMode,
+        child: isEditMode ? _editableValue() : ReadOnlyValue(displayValue),
+        onChanged: _toggleEditMode);
   }
 }
