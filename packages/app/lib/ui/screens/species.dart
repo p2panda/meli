38264bf67861054d551099c237a3696425ad025f
adz <x12@adz.garden>
2023-10-18 22:30:57 +0200

    Add background and title

--- packages/app/lib/ui/screens/species.dart
+++ packages/app/lib/ui/screens/species.dart
@@ -26,6 +26,7 @@ class _SpeciesScreenState extends State<SpeciesScreen> {
   Widget build(BuildContext context) {
     return MeliScaffold(
         title: AppLocalizations.of(context)!.speciesScreenTitle,
+        appBarColor: MeliColors.peach,
         body: Container(
           child: SingleChildScrollView(
             child: Query(
@@ -94,8 +95,12 @@ class _SpeciesProfileState extends State<SpeciesProfile> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(20.0),
+      padding: const EdgeInsets.only(
+          top: 10.0, right: 20.0, bottom: 20.0, left: 20.0),
+      decoration: const SeaWavesBackground(),
       child: Wrap(runSpacing: 20.0, children: [
+        SpeciesProfileTitle(species.species.name),
+        SizedBox(height: 100.0),
         SpeciesField(
           species.species,
           allowNull: false,
@@ -106,5 +111,58 @@ class _SpeciesProfileState extends State<SpeciesProfile> {
             onUpdate: _updateDescription),
       ]),
     );
+  }
+}
+
+class SpeciesProfileTitle extends StatelessWidget {
+  final String title;
+
+  SpeciesProfileTitle(this.title, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(children: [
+      Text(title,
+          textAlign: TextAlign.center,
+          overflow: TextOverflow.ellipsis,
+          maxLines: 2,
+          style: const TextStyle(
+              height: 1.1, fontFamily: 'Staatliches', fontSize: 24.0)),
+    ]));
+  }
+}
+
+class SeaWavesBackground extends Decoration {
+  const SeaWavesBackground();
+
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) {
+    return _SeaWavesPainer();
+  }
+}
+
+class _SeaWavesPainer extends BoxPainter {
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    final Size? bounds = configuration.size;
+
+    final paint = Paint()
+      ..color = MeliColors.peach
+      ..style = PaintingStyle.fill;
+
+    final path = Path();
+    path.moveTo(0, 0);
+    path.lineTo(bounds!.width / 2, 100.0);
+    path.lineTo(bounds.width, 0.0);
+    path.moveTo(0, 0);
+    path.lineTo(bounds.width / 6, 70.0);
+    path.lineTo(bounds.width, 0.0);
+    path.moveTo(0, 0);
+    path.lineTo(bounds.width - (bounds.width / 6), 70.0);
+    path.lineTo(bounds.width, 0.0);
+    path.close();
+
+    canvas.drawPath(path.shift(offset), paint);
   }
 }
