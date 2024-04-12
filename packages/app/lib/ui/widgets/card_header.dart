7e63960cace5f6cc69fc3d834c33826dc81ed543
Andrew Chou <andrewchou@fastmail.com>
2024-04-12 13:12:17 -0400

    add optional onPress prop to CardHeader

--- packages/app/lib/ui/widgets/card_header.dart
+++ packages/app/lib/ui/widgets/card_header.dart
@@ -5,8 +5,9 @@ import 'package:app/ui/colors.dart';
 class MeliCardHeader extends StatelessWidget {
   final String title;
   final Widget? icon;
+  final VoidCallback? onPress;
 
-  MeliCardHeader({super.key, required this.title, this.icon});
+  MeliCardHeader({super.key, required this.title, this.icon, this.onPress});
 
   Widget _title() {
     return Text(
@@ -24,25 +25,29 @@ class MeliCardHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.fromBorderSide(BorderSide(
-            width: 6.0,
-            strokeAlign: BorderSide.strokeAlignCenter,
-            color: MeliColors.pink,
-          )),
-          borderRadius: BorderRadius.all(Radius.circular(12.0))),
-      child: Container(
-        alignment: AlignmentDirectional.centerStart,
-        padding: EdgeInsets.symmetric(
-            vertical: this.icon != null ? 9.0 : 14.0, horizontal: 16.0),
-        child:
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-          this._title(),
-          this._icon(),
-        ]),
-      ),
-    );
+    return Material(
+        child: GestureDetector(
+            onTap: onPress,
+            child: Container(
+              decoration: BoxDecoration(
+                  // color: Colors.white,
+                  border: Border.fromBorderSide(BorderSide(
+                    width: 6.0,
+                    strokeAlign: BorderSide.strokeAlignCenter,
+                    color: MeliColors.pink,
+                  )),
+                  borderRadius: BorderRadius.all(Radius.circular(12.0))),
+              child: Container(
+                alignment: AlignmentDirectional.centerStart,
+                padding: EdgeInsets.symmetric(
+                    vertical: this.icon != null ? 9.0 : 14.0, horizontal: 16.0),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      this._title(),
+                      this._icon(),
+                    ]),
+              ),
+            )));
   }
 }
