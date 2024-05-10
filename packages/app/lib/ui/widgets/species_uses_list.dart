d12049b508d1e2df419d097d848dab3fd928a5ef
Sam Andreae <contact@samandreae.com>
2024-05-10 16:34:03 +0100

    Remove unused imports

--- packages/app/lib/ui/widgets/species_uses_list.dart
+++ packages/app/lib/ui/widgets/species_uses_list.dart
@@ -1,11 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 import 'package:app/models/sightings.dart';
-import 'package:app/models/used_for.dart';
-import 'package:app/ui/widgets/card.dart';
 import 'package:app/ui/widgets/pagination_list.dart';
-import 'package:app/ui/widgets/tag_item.dart';
-import 'package:app/ui/widgets/used_for_dedup_tag_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
