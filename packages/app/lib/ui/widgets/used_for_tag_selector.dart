d82e38bdbe3bcf12897e9574922b1723fcafcfec
Sam Andreae <contact@samandreae.com>
2024-05-10 15:17:47 +0100

    Remove unused import

--- packages/app/lib/ui/widgets/used_for_tag_selector.dart
+++ packages/app/lib/ui/widgets/used_for_tag_selector.dart
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 import 'package:app/ui/widgets/used_for_dedup_tag_list.dart';
-import 'package:app/ui/widgets/pagination_list.dart';
 import 'package:flutter/material.dart';
 
 import 'package:app/models/base.dart';
