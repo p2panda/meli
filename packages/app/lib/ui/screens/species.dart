ebbf1a92a618c9fe7a4a1499f84438ae05729510
Sam Andreae <contact@samandreae.com>
2024-05-10 16:33:39 +0100

    Implement SpeciesUsesAggregate widget

--- packages/app/lib/ui/screens/species.dart
+++ packages/app/lib/ui/screens/species.dart
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+import 'package:app/ui/widgets/species_uses_aggregate.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
@@ -114,6 +115,8 @@ class _SpeciesProfileState extends State<SpeciesProfile> {
             const SizedBox(height: 20.0),
           ])),
           RelatedSightings(id: species.id),
+          const SliverToBoxAdapter(child: SizedBox(height: 20.0)),
+          SliverToBoxAdapter(child: SpeciesUsesAggregate(id: species.id)),
           const SliverToBoxAdapter(child: SizedBox(height: 20.0)),
         ],
       ),
---
+++ packages/app/lib/ui/widgets/species_uses_aggregate.dart
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter/material.dart';
+
+import 'package:app/io/p2panda/publish.dart';
+import 'package:app/models/sightings.dart';
+import 'package:app/models/used_for.dart';
+import 'package:app/ui/widgets/species_uses_list.dart';
+import 'package:app/ui/widgets/tag_item.dart';
+import 'package:app/ui/widgets/used_for_dedup_tag_list.dart';
+import 'package:app/ui/widgets/card.dart';
+import 'package:app/ui/widgets/card_header.dart';
+
+class SpeciesUsesAggregate extends StatelessWidget {
+  final DocumentId id;
+
+  const SpeciesUsesAggregate({super.key, required this.id});
+
+  Widget _builder(List<Sighting> collection) {
+    return MeliCard(
+        elevation: 0,
+        borderWidth: 3.0,
+        child: DeduplicatedUsedForTagsList(
+            builder: (UsedFor usedFor) {
+              return Container(
+                  padding: const EdgeInsets.only(bottom: 5.0),
+                  child:
+                      TagItem(label: usedFor.usedFor, onClick: (item) => {}));
+            },
+            paginator: UsedForPaginator(sighting: collection.first.id)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MeliCard(
+      child: Column(
+        children: [
+          MeliCardHeader(
+            title: AppLocalizations.of(context)!.usedForCardTitle,
+          ),
+          Container(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 18.0),
+              child: SpeciesUsedForList(
+                paginator: SpeciesSightingsPaginator(id),
+                builder: this._builder,
+              )),
+        ],
+      ),
+    );
+  }
+}
