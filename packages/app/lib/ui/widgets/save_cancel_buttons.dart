ee712a9dbfd6bbc2578403947275ee4d26408481
Sam Andreae <contact@samandreae.com>
2024-05-29 15:28:30 +0100

    Style SaveCancel buttons

--- packages/app/lib/ui/widgets/save_cancel_buttons.dart
+++ packages/app/lib/ui/widgets/save_cancel_buttons.dart
@@ -1,3 +1,4 @@
+import 'package:app/ui/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -13,13 +14,31 @@ class SaveCancel extends StatelessWidget {
 
     return Row(children: [
       OverflowBar(
-        spacing: 12,
+        spacing: 10,
         overflowAlignment: OverflowBarAlignment.start,
         children: [
           FilledButton(
-              onPressed: handleSave, child: Text(t.editCardSaveButton)),
+            style: FilledButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                foregroundColor: MeliColors.white,
+                backgroundColor: MeliColors.plum),
+            onPressed: handleSave,
+            child: Text(t.editCardSaveButton),
+          ),
           OutlinedButton(
-              onPressed: handleCancel, child: Text(t.editCardCancelButton))
+              style: OutlinedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                  side: const BorderSide(width: 3.0, color: MeliColors.plum),
+                  foregroundColor: MeliColors.plum),
+              onPressed: handleCancel,
+              child: Text(
+                t.editCardCancelButton,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ))
         ],
       )
     ]);
