7accccacdd79362ec88462b47e8de49104fcee76
Sam Andreae <contact@samandreae.com>
2024-05-29 15:42:14 +0100

    Use SaveCancel widget in HiveLocationField

--- packages/app/lib/ui/widgets/hive_location_field.dart
+++ packages/app/lib/ui/widgets/hive_location_field.dart
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+import 'package:app/ui/widgets/save_cancel_buttons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
@@ -323,37 +324,8 @@ class _HiveLocationFieldEditState extends State<HiveLocationFieldEdit> {
       _additionalInfo(),
       Padding(
           padding: const EdgeInsets.only(top: 10.0),
-          child: Row(children: [
-            OverflowBar(
-              spacing: 10,
-              overflowAlignment: OverflowBarAlignment.start,
-              children: [
-                FilledButton(
-                  style: FilledButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12.0),
-                      ),
-                      foregroundColor: MeliColors.white,
-                      backgroundColor: MeliColors.plum),
-                  onPressed: _handleSave,
-                  child: Text(t.editCardSaveButton),
-                ),
-                OutlinedButton(
-                    style: OutlinedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12.0),
-                        ),
-                        side: const BorderSide(
-                            width: 3.0, color: MeliColors.plum),
-                        foregroundColor: MeliColors.plum),
-                    onPressed: _handleCancel,
-                    child: Text(
-                      t.editCardCancelButton,
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ))
-              ],
-            )
-          ])),
+          child:
+              SaveCancel(handleCancel: _handleCancel, handleSave: _handleSave)),
     ]);
   }
 }
