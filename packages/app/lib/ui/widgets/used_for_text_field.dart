64d0063133ba0348bcb0c3a1a060520ffcb5353c
Sam Andreae <contact@samandreae.com>
2024-05-29 16:29:49 +0100

    Remove unused import

--- packages/app/lib/ui/widgets/used_for_text_field.dart
+++ packages/app/lib/ui/widgets/used_for_text_field.dart
@@ -2,7 +2,6 @@
 
 import 'package:app/ui/widgets/save_cancel_buttons.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class UsedForTextField extends StatefulWidget {
   final void Function(String) submit;
