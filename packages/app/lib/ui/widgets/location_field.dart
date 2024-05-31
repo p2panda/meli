d32f9c2301abf6f8862e72c9902cbba109e35c89
Andrew Chou <andrewchou@fastmail.com>
2024-05-31 12:17:52 -0400

    use shared widgets

--- packages/app/lib/ui/widgets/location_field.dart
+++ packages/app/lib/ui/widgets/location_field.dart
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+import 'package:app/ui/widgets/editable_card.dart';
+import 'package:app/ui/widgets/save_cancel_buttons.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
-import 'package:app/ui/widgets/card.dart';
-import 'package:app/ui/widgets/card_action_button.dart';
-import 'package:app/ui/widgets/card_header.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:maplibre_gl/maplibre_gl.dart';
@@ -28,17 +27,15 @@ class LocationField extends StatefulWidget {
   State<LocationField> createState() => _LocationFieldState();
 }
 
-enum InputMode { read, edit }
-
 double DEFAULT_ZOOM = 10;
 String CURRENT_FEATURE_ID = 'current';
 String TARGET_FEATURE_ID = 'target';
 
 class _LocationFieldState extends State<LocationField> {
-  InputMode _inputMode = InputMode.read;
-
   late double _lat;
   late double _lon;
+
+  bool _isEditMode = false;
 
   MaplibreMapController? _mapController;
   Circle? _currentMapMarker;
@@ -54,61 +51,36 @@ class _LocationFieldState extends State<LocationField> {
   Widget build(BuildContext context) {
     final t = AppLocalizations.of(context)!;
 
-    return MeliCard(
-        child: Column(
-      children: [
-        MeliCardHeader(
-            title: t.sightingLocationFieldTitle,
-            icon: _inputMode == InputMode.read
-                ? CardActionButton(
-                    icon: const Icon(Icons.edit_outlined),
-                    onPressed: _handleStartEditMode)
-                : null),
-        Container(
-            padding:
-                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 6.0),
-            alignment: AlignmentDirectional.centerStart,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                _renderMap(),
-                const SizedBox(
-                  height: 20,
-                ),
-                Text(t.sightingLocationFieldCoordinates(_lat, _lon),
-                    style: const TextStyle(
-                      fontSize: 16.0,
-                    )),
-                if (_inputMode == InputMode.edit) ...[
-                  const SizedBox(height: 12),
-                  Row(children: [
-                    OverflowBar(
-                      spacing: 12,
-                      overflowAlignment: OverflowBarAlignment.start,
-                      children: [
-                        FilledButton(
-                            onPressed: _handleSave,
-                            child: Text(
-                              t.sightingLocationFieldSave,
-                            )),
-                        OutlinedButton(
-                            onPressed: _handleCancel,
-                            child: Text(
-                              t.sightingLocationFieldCancel,
-                            ))
-                      ],
-                    )
-                  ])
-                ]
-              ],
-            ))
-      ],
-    ));
+    return EditableCard(
+      title: t.sightingLocationFieldTitle,
+      isEditMode: _isEditMode,
+      onChanged: _isEditMode
+          ? _handleCancel
+          : () {
+              setState(() {
+                _isEditMode = true;
+              });
+            },
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        _renderMap(),
+        const SizedBox(
+          height: 20,
+        ),
+        Text(t.sightingLocationFieldCoordinates(_lat, _lon),
+            style: const TextStyle(
+              fontSize: 16.0,
+            )),
+        if (_isEditMode) ...[
+          Padding(
+              padding: const EdgeInsets.only(top: 10.0),
+              child: SaveCancelButtons(
+                  handleCancel: _handleCancel, handleSave: _handleSave)),
+        ]
+      ]),
+    );
   }
 
   Widget _renderMap() {
-    bool enableInteractions = _inputMode == InputMode.edit;
-
     return Container(
         alignment: Alignment.center,
         height: 200,
@@ -116,10 +88,10 @@ class _LocationFieldState extends State<LocationField> {
             initialCameraPosition: CameraPosition(
                 target: LatLng(widget.latitude, widget.longitude),
                 zoom: DEFAULT_ZOOM),
-            scrollGesturesEnabled: enableInteractions,
-            dragEnabled: enableInteractions,
-            zoomGesturesEnabled: enableInteractions,
-            trackCameraPosition: enableInteractions,
+            scrollGesturesEnabled: _isEditMode,
+            dragEnabled: _isEditMode,
+            zoomGesturesEnabled: _isEditMode,
+            trackCameraPosition: _isEditMode,
             compassEnabled: false,
             rotateGesturesEnabled: false,
             onMapCreated: (controller) {
@@ -136,7 +108,7 @@ class _LocationFieldState extends State<LocationField> {
                   geometry: LatLng(widget.latitude, widget.longitude)));
             },
             onMapClick: (_, latLng) async {
-              if (_inputMode == InputMode.read) return;
+              if (!_isEditMode) return;
               if (_currentMapMarker == null) return;
 
               var existingNextMarker = _mapController!.circles.where((c) {
@@ -173,7 +145,7 @@ class _LocationFieldState extends State<LocationField> {
     }
 
     setState(() {
-      _inputMode = InputMode.read;
+      _isEditMode = false;
       widget.onUpdate(latitude: _lat, longitude: _lon);
     });
 
@@ -184,16 +156,9 @@ class _LocationFieldState extends State<LocationField> {
     await _resetMap(LatLng(widget.latitude, widget.longitude));
 
     setState(() {
-      _inputMode = InputMode.read;
-
+      _isEditMode = false;
       _lat = widget.latitude;
       _lon = widget.longitude;
-    });
-  }
-
-  void _handleStartEditMode() {
-    setState(() {
-      _inputMode = InputMode.edit;
     });
   }
 
