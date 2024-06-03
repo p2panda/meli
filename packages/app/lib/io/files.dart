59b4ecded37a9fb854a5e3111f7e944aae53d4df
adz <x12@adz.garden>
2024-06-03 20:12:39 +0200

    Export images

--- packages/app/lib/io/files.dart
+++ packages/app/lib/io/files.dart
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart' as provider;
 
 /// Blobs base path.
@@ -8,4 +11,14 @@ const String BLOBS_BASE_PATH = 'http://localhost:2020/blobs';
 Future<String> get applicationSupportDirectory async {
   final directory = await provider.getApplicationSupportDirectory();
   return directory.path;
+}
+
+Future<void> downloadAndExportImages(
+    List<String> blobIds, String targetDirectory) async {
+  for (var id in blobIds) {
+    final http.Response response =
+        await http.get(Uri.parse("$BLOBS_BASE_PATH/$id"));
+    final file = File("$targetDirectory/$id.jpg");
+    await file.writeAsBytes(response.bodyBytes);
+  }
 }
